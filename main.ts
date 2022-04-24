6472a81187a4110c68e476ffdd3ad029d391a193
lvkgamer6307 <90861934+lvkgamer6307@users.noreply.github.com>
2022-04-24 17:00:17 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -3,4 +3,22 @@ basic.forever(function () {
     radio.sendValue("mgx", input.acceleration(Dimension.X))
     radio.sendValue("mgy", input.acceleration(Dimension.Y))
     radio.sendValue("mgz", input.acceleration(Dimension.Z))
+    if (input.acceleration(Dimension.X) < input.acceleration(Dimension.Y)) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (input.acceleration(Dimension.Y) < input.acceleration(Dimension.X)) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
 })
